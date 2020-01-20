f99388931af9fba44f28f718f05def1721a53e5e
Sam <super8989@gmail.com>
2020-01-20 18:13:57 -0500

    added in signin and signup link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { Box } from 'gestalt';
+import { Box, Text } from 'gestalt';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
 	<Box height={70} color='midnight' padding={1} shape='roundedBottom'>
-		{/* Sign inLink */}
+		{/* Sign In Link */}
+		<NavLink to='/signin'>
+			<Text size='xl' color='white'>
+				Sign In
+			</Text>
+		</NavLink>
+
+		{/* Sign Up Link */}
+		<NavLink to='/signup'>
+			<Text size='xl' color='white'>
+				Sign Up
+			</Text>
+		</NavLink>
 	</Box>
 );
 
