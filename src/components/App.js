b96145d656ee9f8b4c64b38c8cd1cddd42f4676a
Sam <super8989@gmail.com>
2020-01-22 20:51:20 -0500

    filter search based on brand name

--- src/components/App.js
+++ src/components/App.js
@@ -52,8 +52,14 @@ class App extends Component {
 		this.setState({ searchTerm: value });
 	};
 
+	filteredBrands = ({ searchTerm, brands }) => {
+		return brands.filter(brand => {
+			return brand.name.toLowerCase().includes(searchTerm.toLowerCase());
+		});
+	};
+
 	render() {
-		const { brands, searchTerm } = this.state;
+		const { searchTerm } = this.state;
 
 		return (
 			<Container>
@@ -94,7 +100,7 @@ class App extends Component {
 					display='flex'
 					justifyContent='around'
 				>
-					{brands.map(brand => (
+					{this.filteredBrands(this.state).map(brand => (
 						<Box paddingY={4} margin={2} width={200} key={brand._id}>
 							<Card
 								image={
