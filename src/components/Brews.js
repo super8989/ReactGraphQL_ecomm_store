813474b77ebe1345cdf67c4a4e526e4c7e544143
Sam <super8989@gmail.com>
2020-01-23 21:17:24 -0500

    log match.params.brandId in Brews.js

--- src/components/Brews.js
+++ src/components/Brews.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Brews extends Component {
+	componentDidMount() {
+		console.log(this.props.match.params.brandId);
+	}
+
 	render() {
 		return <div>Brews</div>;
 	}
