17e398f2cb2be75f2da54b867cbcd0ba30219d2e
Sam <super8989@gmail.com>
2020-01-23 21:24:39 -0500

    request brews by brandId in brews component

--- src/components/Brews.js
+++ src/components/Brews.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import Strapi from 'strapi-sdk-javascript/build/main';
+
+const apiUrl = process.env.API_URL || 'http://localhost:1337/';
+const strapi = new Strapi(apiUrl);
 
 class Brews extends Component {
-	componentDidMount() {
-		console.log(this.props.match.params.brandId);
+	async componentDidMount() {
+		// console.log(this.props.match.params.brandId);
+		try {
+			const response = await strapi.request('POST', '/graphql', {
+				data: {
+					query: `query {
+            brand(id: "${this.props.match.params.brandId}") {
+              _id
+              name
+              brews {
+                _id
+                name
+                image {
+                  url
+                }
+                price
+                
+              }
+            }
+          }
+          `
+				}
+			});
+			console.log(response);
+		} catch (err) {
+			console.error(err);
+		}
 	}
 
 	render() {
