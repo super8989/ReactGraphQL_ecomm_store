8487df4085e8a5d3f4ac4a8784943c3343457785
Sam <super8989@gmail.com>
2020-01-26 12:58:37 -0500

    Heading for the signup form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import { Container, Box, Button, Heading, Text, TextField } from 'gestalt';
 
 class Signup extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Signup</h1>
-			</div>
+			<Container>
+				<Box
+					dangerouslySetInlineStyle={{
+						__style: {
+							backgroundcolor: '#ebe2da'
+						}
+					}}
+					margin={4}
+					padding={4}
+					shape='rounded'
+					display='flex'
+					justifyContent='center'
+				>
+					{/* Sign Up Form */}
+					<form
+						style={{
+							display: 'inlineBlock',
+							textAlign: 'center',
+							maxWidth: 450
+						}}
+					>
+						{/* Sign Up Form Heading */}
+						<Box
+							marginBottom={2}
+							display='flex'
+							direction='column'
+							alignItems='center'
+						>
+							<Heading color='midnight'>Let's Get Started</Heading>
+							<Text italic color='olive'>
+								Sign up to order some brews!
+							</Text>
+						</Box>
+					</form>
+				</Box>
+			</Container>
 		);
 	}
 }
