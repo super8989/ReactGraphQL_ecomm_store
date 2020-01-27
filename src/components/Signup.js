4ab89789854011a3b9e68539d68a27cc02746a94
Sam <super8989@gmail.com>
2020-01-27 18:51:49 -0500

    created ToastMessage component to show upon validation error

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Container, Box, Button, Heading, Text, TextField } from 'gestalt';
 
+import ToastMessage from './ToastMessage';
+
 class Signup extends Component {
 	state = {
 		username: '',
 		email: '',
-		password: ''
+		password: '',
+		toast: false,
+		toastMessage: ''
 	};
 
 	handleChange = ({ event, value }) => {
@@ -17,16 +21,25 @@ class Signup extends Component {
 	handleSubmit = event => {
 		event.preventDefault();
 
-		if (!this.isFormEmpty(this.state)) {
-			console.log('submitted');
+		if (this.isFormEmpty(this.state)) {
+			this.showToast('Fill in all fields');
+			return;
 		}
+		console.log('submitted');
 	};
 
 	isFormEmpty = ({ username, email, password }) => {
 		return !username || !email || !password;
 	};
 
+	showToast = toastMessage => {
+		this.setState({ toast: true, toastMessage });
+		setTimeout(() => this.setState({ toast: false, toastMessage: '' }), 3000);
+	};
+
 	render() {
+		const { toastMessage, toast } = this.state;
+
 		return (
 			<Container>
 				<Box
@@ -89,6 +102,7 @@ class Signup extends Component {
 						<Button inline color='blue' text='Submit' type='submit' />
 					</form>
 				</Box>
+				<ToastMessage show={toast} message={toastMessage} />
 			</Container>
 		);
 	}
