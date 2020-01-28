06ae138817049e43679a0c62b667089be8a0e4ba
Sam <super8989@gmail.com>
2020-01-28 11:02:34 -0500

    change Navbar upon signup

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,6 +23,13 @@ export const getCart = (cartKey = CART_KEY) => {
 };
 
 // Auth
+export const getToken = (tokenKey = TOKEN_KEY) => {
+	if (localStorage && localStorage.getItem(tokenKey)) {
+		return JSON.parse(localStorage.getItem(tokenKey));
+	}
+	return null;
+};
+
 export const setToken = (value, tokenKey = TOKEN_KEY) => {
 	if (localStorage) {
 		localStorage.setItem(tokenKey, JSON.stringify(value));
