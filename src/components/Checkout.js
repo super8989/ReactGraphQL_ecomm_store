46a8497816a953ca45c3cbc73f3d5b92ec005cf9
Sam <super8989@gmail.com>
2020-01-28 17:05:40 -0500

    show default text if cart is empty

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -60,78 +60,94 @@ class Checkout extends Component {
 				>
 					{/* Checkout Form Heading */}
 					<Heading color='midnight'>Checkout</Heading>
-
-					{/* User Cart */}
-					<Box
-						display='flex'
-						justifyContent='center'
-						alignItems='center'
-						direction='column'
-						marginTop={2}
-						marginBottom={6}
-					>
-						<Text color='darkGray' italic>
-							{cartItems.length} for Checkout
-						</Text>
-						<Box padding={2}>
-							{cartItems.map(item => (
-								<Box key={item._id} padding={1}>
-									<Text color='midnight'>
-										{item.name} x {item.quantity} - $
-										{item.quantity * item.price}
-									</Text>
+					{cartItems.length > 0 ? (
+						<>
+							{/* User Cart */}
+							<Box
+								display='flex'
+								justifyContent='center'
+								alignItems='center'
+								direction='column'
+								marginTop={2}
+								marginBottom={6}
+								color='lightWash'
+								shape='rounded'
+								padding={4}
+							>
+								<Text color='darkGray' italic>
+									{cartItems.length} items for Checkout
+								</Text>
+								<Box padding={2}>
+									{cartItems.map(item => (
+										<Box key={item._id} padding={1}>
+											<Text color='midnight'>
+												{item.name} x {item.quantity} - $
+												{item.quantity * item.price}
+											</Text>
+										</Box>
+									))}
 								</Box>
-							))}
+								<Text weight='bold' color='red'>
+									Total Amount: {calculatePrice(cartItems)}
+								</Text>
+							</Box>
+							{/* Checkout Form */}
+							<form
+								style={{
+									display: 'inlineBlock',
+									textAlign: 'center',
+									maxWidth: 450
+								}}
+								onSubmit={this.handleConfirmOrder}
+							>
+								{/* Shipping Address Input */}
+								<TextField
+									id='address'
+									type='text'
+									name='address'
+									placeholder='Shipping Address'
+									onChange={this.handleChange}
+								/>
+								{/* Postal Code Input */}
+								<TextField
+									id='postalCode'
+									type='text'
+									name='postalCode'
+									placeholder='Postal Code'
+									onChange={this.handleChange}
+								/>
+								{/* City Input */}
+								<TextField
+									id='city'
+									type='text'
+									name='city'
+									placeholder='City of Residence'
+									onChange={this.handleChange}
+								/>
+								{/* Confirmation Email Address Input */}
+								<TextField
+									id='confirmationEmailAddress'
+									type='email'
+									name='confirmationEmailAddress'
+									placeholder='Confirmation Email Address'
+									onChange={this.handleChange}
+								/>
+								<button id='stripe__button' type='submit'>
+									Submit
+								</button>
+							</form>
+						</>
+					) : (
+						// Default text if no items in cart
+						<Box color='lightWash' shape='rounded' padding={4} margin={4}>
+							<Heading align='center' color='watermelon' size='xs'>
+								Your Cart is Empty
+							</Heading>
+							<Text align='center' italic color='red'>
+								Add some brews!
+							</Text>
 						</Box>
-						<Text weight='bold' color='red'>
-							Total Amount: {calculatePrice(cartItems)}
-						</Text>
-					</Box>
-					{/* Checkout Form */}
-					<form
-						style={{
-							display: 'inlineBlock',
-							textAlign: 'center',
-							maxWidth: 450
-						}}
-						onSubmit={this.handleConfirmOrder}
-					>
-						{/* Shipping Address Input */}
-						<TextField
-							id='address'
-							type='text'
-							name='address'
-							placeholder='Shipping Address'
-							onChange={this.handleChange}
-						/>
-						{/* Postal Code Input */}
-						<TextField
-							id='postalCode'
-							type='text'
-							name='postalCode'
-							placeholder='Postal Code'
-							onChange={this.handleChange}
-						/>
-						{/* City Input */}
-						<TextField
-							id='city'
-							type='text'
-							name='city'
-							placeholder='City of Residence'
-							onChange={this.handleChange}
-						/>
-						{/* Confirmation Email Address Input */}
-						<TextField
-							id='confirmationEmailAddress'
-							type='email'
-							name='confirmationEmailAddress'
-							placeholder='Confirmation Email Address'
-							onChange={this.handleChange}
-						/>
-						<button id='stripe__button' type='submit'>
-							Submit
-						</button>
-					</form>
+					)}
 				</Box>
 				<ToastMessage show={toast} message={toastMessage} />
 			</Container>
